228369162e19ca0ea8f16725793a4ea1e1e687b1
KatyaMy <20FamGam20@gmail.com>
2022-04-27 19:50:46 -0400

    Selenium test new version by Stan

---
+++ RE_Olga/test_selenium_1.0.py
@@ -0,0 +1,27 @@
+import unittest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+class TestMePlease(unittest.TestCase):
+
+    def setUp(self) -> None:
+        service = Service(ChromeDriverManager().install())
+        # Initialisation
+        self.driver = webdriver.Chrome(service=service)
+        self.driver.set_window_size(1024, 768)
+
+    def test_1(self):
+        # test
+        self.driver.get("https://www.google.com/")
+
+        #self.assertTrue(False
+
+
+    def tearDown(self) -> None:
+        # destroy driver instance
+        self.driver.close()
+
+if __name__ == '__main__':
+    unittest.main()
